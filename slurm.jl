b63527e8754cb45ae0f72b4fab62709e4b493aaf
lucaslovescode <lucawhit@umich.edu>
2025-11-03 15:14:35 -0700

    updating run to tokenize selfies

--- slurm.jl
+++ slurm.jl
@@ -1,5 +1,7 @@
 include("experiments.jl")
+include("tokenize_selfies.jl")
 
 using JLD
 
+selfies_to_tokens("training.txt","selfies_training")
 main_train(Bag_block, "/home/lucas/OneDrive/Documents/phd1/infer_pcfg_julia") 
